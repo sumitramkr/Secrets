42dc000230d3dd322882fd166bc33a939fa761d6
sumitramkr <sumitramkr77e@gmail.com>
2022-06-20 23:29:15 +0530

    added mongoose encryption

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 app.use(express.static("public"));
