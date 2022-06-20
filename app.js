1e1a32c58d717786a2c5410aaca27671977b1b48
sumitramkr <sumitramkr77e@gmail.com>
2022-06-20 14:20:20 +0530

    mongoose setup

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
 //jshint esversion:6
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const app = express();
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+});
+
+const User = new mongoose.model("User", userSchema);
+
+app.get("/", function (req, res) {
+  res.render("home");
+});
+
+app.get("/login", function (req, res) {
+  res.render("login");
+});
+
+app.get("/register", function (req, res) {
+  res.render("register");
+});
+
+app.listen(4100, function () {
+  console.log("Connected on port 4100");
+});
